a24c23713bea89d73096bff6071e8bf76e2806ac
Abhishek Sharma <99042525+Abhi3391@users.noreply.github.com>
2024-02-19 07:49:56 +0530

    Add files via upload

---
+++ delta6labs_training_assignments/react-assignments/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import UserForm from './8-feb-assignment/conditional_rendering/using_class';
+import UserForm1 from './8-feb-assignment/conditional_rendering/using_funtional';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserForm />
+        <UserForm1 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
